26d90aceb2b158ea9e27e17a31a5321a8c3d210a
ZTMM1994 <zenab.tamimy@student.uva.nl>
2017-06-01 17:49:15 +0200

    Network Try out
    Trying out Network code

---
+++ NetworkVV.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 16:40:22 2017
+
+@author: Zenab
+"""
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def draw_graph(graph):
+
+    # extract nodes from graph
+    nodes = set([n1 for n1, n2 in graph] + [n2 for n1, n2 in graph])
+
+    # create networkx graph
+    G=nx.Graph()
+
+    # add nodes
+    for node in nodes:
+        G.add_node(node)
+
+    # add edges
+    for edge in graph:
+        G.add_edge(edge[0], edge[1])
+
+    # draw graph
+    pos = nx.shell_layout(G)
+    nx.draw(G, pos)
+
+    # show graph
+    plt.show()
+
+# draw example
+graph = edges
+draw_graph(graph)
